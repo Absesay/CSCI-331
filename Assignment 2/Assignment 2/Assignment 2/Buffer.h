249f4b2038c8732e001e035fefbd0513be2877d2
mechanic64 <olsonjbenjamin@gmail.com>
2021-02-24 14:05:40 -0600

    02/24/21 Commit and Push

---
+++ Assignment 2/Assignment 2/Assignment 2/Buffer.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std; 
+
+class DelimTextBuffer
+{
+public:
+
+inline int Read(ifstream & infile);
+/*int Read (istream & file);
+int Write(ostream & file);
+int Pack (const char * str, int size = -1);
+int Unpack (char * str);*/
+/*
+private: 
+char Delim; // delimiter c t hold field values
+char* Buffer; // character ~rrayf oacked fields
+int Buffersize;
+int MaxBytes; // current size o p i'n the in . umber of characters ,. int MaxBytes; / I maximum n
+
+ // packing/unpacking position in buffer
+    int NextByte; */
+};
+    
