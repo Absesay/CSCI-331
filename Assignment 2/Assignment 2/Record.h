2a6601ce5154817b95d7e16ed1c607f956d9cb5c
Ben Olson <olsonjbenjamin@gmail.com>
2021-02-24 14:42:25 -0600

    Readding work to tree

---
+++ Assignment 2/Assignment 2/Record.h
@@ -0,0 +1,26 @@
+///*************************************************************
+/// @author Authors: Ben Olson,Gia Dat Diep
+///					 Moffat Omuya, Sandesh Raj and Abraham Sesay
+/// 
+/// Description of project goes here
+/// 
+/// 
+///*************************************************************
+
+
+#include <cstring>
+#include <cstdlib>
+#include <string>
+
+class Record
+{
+	int zipCode = 0;
+	String placeName = " ";
+	String State = " ";
+	String County = " ";
+	float Lat = 0;
+	float Long = 0;
+
+	Record(){};
+
+};
